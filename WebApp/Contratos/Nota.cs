f2949391ee8e801b730ec34014754b9e31f2dd2a
Maxi Lovera <maximilianolovera@gmail.com>
2019-05-27 17:12:15 -0300

    Nuevos metodos

---
+++ WebApp/Contratos/Nota.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Contratos
+{
+    public class Nota
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime? FechaEventoAsociado { get; set; }
+        public bool Leida { get; set; }
+        public Comentario[] Comentarios { get; set; }
+    }
+}
