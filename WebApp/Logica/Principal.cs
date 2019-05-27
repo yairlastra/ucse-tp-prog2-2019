cbd9924e5f7a465d79ba2fd3d2e774eaf98295dc
lucasbelmonteamenta2.0@gmail.com <lucasbelmonteamenta2.0@gmail.com>
2019-05-27 19:28:01 -0300

    agregue el proyecto logica y la clase prinsipal (cosas que el profe dijo que despes abisaria por slack que hay que poner)

---
+++ WebApp/Logica/Principal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contratos;
+
+namespace Logica
+{
+    public class Principal
+    {
+
+
+    }
+}
