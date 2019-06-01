6d3691b927f9ed8685863e33b4bfd8fbd7258dd9
lucasbelmonteamenta2.0@gmail.com <lucasbelmonteamenta2.0@gmail.com>
2019-06-01 01:23:35 -0300

    cree metodos basicos para crear, cargar, modificar y convocar los archivos (json) de los hijos, los padres, los docentes y las instituciones.

--- WebApp/Logica/Principal.cs
+++ WebApp/Logica/Principal.cs
@@ -11,35 +11,22 @@ namespace Logica
 {
     public sealed class Principal
     {
-        private static Principal instancia = null;
+        private readonly static Principal instancia = new Principal();
 
-        public static Principal Instancia
-        {
-            get
-            {
-                if (instancia == null)
-                { instancia = new Principal(); }
-                return instancia;
-            }
-        }
+        public static Principal Instancia { get { return instancia; } }
 
-        private Principal()
-        {
-            this.Instituciones = new Institucion[]{};
+        private Principal() { }
 
-        }
 
         public static string RutaArchivoInstituciones  = @" C:\Users\lu_ga\Documents\prog1-tp-net-2018\trabajoPracticoNet\archivos\Instituciones.txt";
-           
+        public static string RutaArchivoHijos = @" C:\Users\lu_ga\Documents\prog1-tp-net-2018\trabajoPracticoNet\archivos\Hijos.txt";
+        public static string RutaArchivoPadres = @" C:\Users\lu_ga\Documents\prog1-tp-net-2018\trabajoPracticoNet\archivos\Padres.txt";
+        public static string RutaArchivoDocentes = @" C:\Users\lu_ga\Documents\prog1-tp-net-2018\trabajoPracticoNet\archivos\Docentes.txt";
 
-        public Institucion[] Instituciones { get; set; }
-        public Hijo[] Hijos { get; set; }
-        public Padre[] Padres { get; set; }
-        public Docente[] Docentes { get; set; }
-        public Usuario[] Usuarios { get; set; }
 
         public Institucion[] ObtenerInstituciones()
         {
+            Institucion[] Instituciones = new Institucion[] {};
             if (!File.Exists(RutaArchivoInstituciones))
             { File.Create(RutaArchivoInstituciones); }
             else
@@ -53,6 +40,93 @@ namespace Logica
             return Instituciones;
         }
 
+        public Hijo[] ObtenerHijos()
+        {
+            Hijo[] Hijos = new Hijo[] {};
+            if (!File.Exists(RutaArchivoHijos))
+            { File.Create(RutaArchivoHijos); }
+            else
+            {
+                using (StreamReader Leer = new StreamReader(RutaArchivoHijos, false))
+                {
+                    string ContenidoDelArchivo = Leer.ReadToEnd();
+                    Hijos = JsonConvert.DeserializeObject<Hijo[]>(ContenidoDelArchivo) == null ? new Hijo[] { } : JsonConvert.DeserializeObject<Hijo[]>(ContenidoDelArchivo);
+                }
+            }
+            return Hijos;
+        }
+
+        public Padre[] ObtenerPadres()
+        {
+            Padre[] Padres = new Padre[] {};
+            if (!File.Exists(RutaArchivoPadres))
+            { File.Create(RutaArchivoPadres); }
+            else
+            {
+                using (StreamReader Leer = new StreamReader(RutaArchivoPadres, false))
+                {
+                    string ContenidoDelArchivo = Leer.ReadToEnd();
+                    Padres = JsonConvert.DeserializeObject<Padre[]>(ContenidoDelArchivo) == null ? new Padre[] { } : JsonConvert.DeserializeObject<Padre[]>(ContenidoDelArchivo);
+                }
+            }
+            return Padres;
+        }
+
+        public Docente[] ObtenerDocentes()
+        {
+            Docente[] Docentes = new Docente[] {};
+            if (!File.Exists(RutaArchivoDocentes))
+            { File.Create(RutaArchivoDocentes); }
+            else
+            {
+                using (StreamReader Leer = new StreamReader(RutaArchivoDocentes, false))
+                {
+                    string ContenidoDelArchivo = Leer.ReadToEnd();
+                    Docentes = JsonConvert.DeserializeObject<Docente[]>(ContenidoDelArchivo) == null ? new Docente[] { } : JsonConvert.DeserializeObject<Docente[]>(ContenidoDelArchivo);
+                }
+            }
+            return Docentes;
+        }
+
+        public bool ModificarArchivoInstituciones()
+        {
+            using (StreamWriter Escribir = new StreamWriter(RutaArchivoInstituciones, false))
+            {
+                string jsonInstituciones = JsonConvert.SerializeObject(ObtenerInstituciones());
+                Escribir.Write(jsonInstituciones);
+                return true;
+            }
+        }
+
+        public bool ModificarArchivoHijos()
+        {
+            using (StreamWriter Escribir = new StreamWriter(RutaArchivoHijos, false))
+            {
+                string jsonHijos = JsonConvert.SerializeObject(ObtenerHijos());
+                Escribir.Write(jsonHijos);
+                return true;
+            }
+        }
+
+        public bool ModificarArchivoPadres()
+        {
+            using (StreamWriter Escribir = new StreamWriter(RutaArchivoPadres, false))
+            {
+                string jsonPadres = JsonConvert.SerializeObject(ObtenerPadres());
+                Escribir.Write(jsonPadres);
+                return true;
+            }
+        }
+
+        public bool ModificarArchivoDocentes()
+        {
+            using (StreamWriter Escribir = new StreamWriter(RutaArchivoDocentes, false))
+            {
+                string jsonDocentes = JsonConvert.SerializeObject(ObtenerDocentes());
+                Escribir.Write(jsonDocentes);
+                return true;
+            }
+        }
 
 
 
