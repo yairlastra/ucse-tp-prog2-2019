57b14e2d759eaef23e61bf2d44f88f15d1ef9343
Maxi Lovera <maximilianolovera@gmail.com>
2019-06-11 11:04:47 -0300

    Pantalla cuaderno: - Muestra la lista de hijos para el padre - Para docente o directora muestra un selector de sala, una vez que el usuario selecciona la sala muestra los alumnos de la sala seleccionada.
    Al seleccionar un alumno, lleva a la pantalla de cuaderno, es decir, el listado de notas del alumno seleccionado, donde cualquier usuario puede generar un comentario (responder nota) y si el usuario es de rol padre puede marcar la nota como leída.

---
+++ WebApp/WebApp/Controllers/CuadernoController.cs
@@ -0,0 +1,78 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web;
+using System.Collections.Generic;
+using System;
+using System.Net.Mail;
+using WebApp.Controllers;
+using Contratos;
+using Mocks;
+using WebApp.Models;
+
+namespace AnBem.WebApplication.Controllers
+{
+    public class CuadernoController : BaseController
+    {
+        private static IServicioWeb servicio = new MockService();
+
+        [HttpGet]
+        public async Task<ActionResult> Index(int? classroomId = null, int? studenId = null)
+        {
+            if (usuarioLogueado.RolSeleccionado != Roles.Padre)
+            {
+                ViewBag.Salas = new SelectList(servicio.ObtenerSalasPorInstitucion(usuarioLogueado), "Id", "Nombre", classroomId);
+            }
+
+            var alumnos = servicio.ObtenerPersonas(usuarioLogueado);
+
+            if (classroomId != null)
+            {
+                alumnos = alumnos.Where(x => x.Sala.Id == classroomId).ToArray();
+            }
+
+            ViewBag.Hijos = alumnos;
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Book(int id)
+        {
+            var alumno = servicio.ObtenerPersonas(usuarioLogueado).SingleOrDefault(x=>x.Id == id);
+
+            if (alumno == null) { 
+                return RedirectToAction("Index");
+            }
+
+            alumno.Notas = servicio.ObtenerCuadernoComunicaciones(alumno.Id, usuarioLogueado);
+
+            return View(alumno);
+        }
+
+        [HttpPost]
+        public JsonResult MarcarLeida(int id)
+        {
+            var rta = servicio.MarcarNotaComoLeida(new Nota() { Id = id }, usuarioLogueado);
+
+            return Json(rta);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public JsonResult ResponderNota(int id, string mensaje)
+        {
+            Comentario comentario = new Comentario()
+            {
+                Fecha = DateTime.Now,
+                Mensaje = mensaje,
+                Usuario = new Usuario() { Nombre = usuarioLogueado.Nombre, Apellido = usuarioLogueado.Apellido, Email = usuarioLogueado.Email  }
+            };
+
+            var rta = servicio.ResponderNota(new Nota() { Id = id }, comentario, usuarioLogueado);
+
+            return Json(rta);
+        }
+    }
+}
